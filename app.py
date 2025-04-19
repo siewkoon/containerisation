bf1ffe7863f531c625ae56a8159e9805c0616538
siewkoon <84426971+siewkoon@users.noreply.github.com>
2025-04-19 15:50:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,13 +36,15 @@ model = LinearRegression().fit(X, y)
 @app.route("/predict")
 def predict():
     x = float(request.args.get("x", 0))
-    y_pred = model.predict([[x]])[0]
+    w = float(request.args.get("w", 0))
+    
+    y_pred = model.predict([[x, w]])[0]
     
     # Log prediction
     with open("output.txt", "w") as f:
         f.write(f"Input x: {x}\nPrediction: {y_pred}\n")
     
-    return jsonify({"x": x, "prediction": y_pred})
+    return jsonify({"x": x, "w":w, "prediction": y_pred})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
